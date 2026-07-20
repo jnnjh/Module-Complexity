de763a97debf9e6ef36604c6bc6c5fccd5a68a10
jnnjh <joannejhoieacar@gmail.com>
2026-07-20 21:09:30 +0200

    add memoization to making_change

--- Sprint-2/improve_with_caches/making_change/making_change.py
+++ Sprint-2/improve_with_caches/making_change/making_change.py
@@ -1,32 +1,40 @@
-from typing import List
+from typing import List, Tuple
+
+cache = {}
 
 
 def ways_to_make_change(total: int) -> int:
     """
-    Given access to coins with the values 1, 2, 5, 10, 20, 50, 100, 200, returns a count of all of the ways to make the passed total value.
-
-    For instance, there are two ways to make a value of 3: with 3x 1 coins, or with 1x 1 coin and 1x 2 coin.
+    Given access to coins with the values 1, 2, 5, 10, 20, 50, 100, 200,
+    returns the number of ways to make the passed total value.
     """
-    return ways_to_make_change_helper(total, [200, 100, 50, 20, 10, 5, 2, 1])
+    cache.clear()
+    return ways_to_make_change_helper(
+        total, (200, 100, 50, 20, 10, 5, 2, 1)
+    )
 
 
-def ways_to_make_change_helper(total: int, coins: List[int]) -> int:
-    """
-    Helper function for ways_to_make_change to avoid exposing the coins parameter to callers.
-    """
-    if total == 0 or len(coins) == 0:
+def ways_to_make_change_helper(total: int, coins: Tuple[int, ...]) -> int:
+    key = (total, coins)
+
+    if key in cache:
+        return cache[key]
+
+    if total == 0:
+        return 1
+
+    if total < 0 or len(coins) == 0:
         return 0
 
     ways = 0
-    for coin_index in range(len(coins)):
-        coin = coins[coin_index]
-        count_of_coin = 1
-        while coin * count_of_coin <= total:
-            total_from_coins = coin * count_of_coin
-            if total_from_coins == total:
-                ways += 1
-            else:
-                intermediate = ways_to_make_change_helper(total - total_from_coins, coins=coins[coin_index+1:])
-                ways += intermediate
-            count_of_coin += 1
+
+    coin = coins[0]
+    remaining = coins[1:]
+
+    amount = total
+    while amount >= 0:
+        ways += ways_to_make_change_helper(amount, remaining)
+        amount -= coin
+
+    cache[key] = ways
     return ways
